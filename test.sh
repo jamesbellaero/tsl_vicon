5331fb441600b586f1fa361a151aa749801d6434
James Bell <jtb2013@gmail.com>
2018-01-13 16:11:50 -0600

    seems to wrok fine

---
+++ test.sh
@@ -0,0 +1,1 @@
+rostopic pub -1 /orbot_server/orbot_target geometry_msgs/TransformStamped '{header: {stamp: now, frame_id: base_link}, transform: {translation: {x: 0,y: 0,z: 0}, rotation: {x: 0,y: 0,z: 0,w: 0}}}'
