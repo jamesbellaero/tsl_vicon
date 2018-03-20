5372ccce8ff3aef88441fb47ab2774a20797406e
James Bell <jtb2013@gmail.com>
2018-03-20 10:46:34 -0500

    initial commit

--- src/quaternion.h
+++ src/quaternion.h
@@ -7,6 +7,10 @@ struct Vec3{
 	float v[3]={0};
 };
 
+struct DCM{
+	float v[3][3]={0}{0};
+}
+
 Vec3 Quat2RPY(Vec4 q){
 	float q0 = q.v[0]; //q0 = q.v[1];
 	float q1 = q.v[1]; //q1 = q.v[2];
@@ -19,4 +23,45 @@ Vec3 Quat2RPY(Vec4 q){
 	RPY.v[2] = atan2(2*(q0*q3 + q1*q2),1 - 2*(q2*q2 + q3*q3) );	//Yaw
 
 	return RPY;
+}
+DCM Quat2DCM(Vec4 q){
+	/*converts quaternion to a Direction Cosine Matrix (rotation matrix)*/
+	DCM M;
+	float q0 = q.v[3];
+	float q1 = q.v[0];
+	float q2 = q.v[1];
+	float q3 = q.v[2];
+	M.v[0][0]=q0*q0+q1*q1-q2*q2-q3*q3;
+	M.v[0][1]=2*(q1*q2-q0*q3);
+	M.v[0][2]=2*(q0*q2+q1*q3);
+	M.v[1][0]=2*(q1*q2+q0*q3);
+	M.v[1][1]=q0*q0-q1*q1+q2*q2-q3*q3;
+	M.v[1][2]=2*(q2*q3-q0*q1);
+	M.v[2][0]=2*(q1*q3-q0*q2);
+	M.v[2][1]=2*(q0*q1+q2*q3);
+	M.v[2][2]=q0*q0-q1*q1-q2*q2+q3*q3;
+}
+
+DCM RotBetweenDCMs(DCM Rri, DCM Rbi){
+	/*Returns rotation from reference to body
+	first matrix is rotation from inertial to reference
+	second matrix is rotation from inertial to body*/
+
+	DCM Rir;//transpose, could just change how it's multiplied below but that's unintuitive
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			Rir.v[i][j]=Rri.v[j][i];
+		}
+	}
+	DCM Rbr;
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			for(int k=0;k<3;k++){
+				Rbr.v[i][j]+=Rbi[i][k]*Rir[k][j];
+			}
+		}
+	}
+	return Rbr;
+
+
 }
---
+++ src/tsl_writer.cpp
@@ -0,0 +1,98 @@
+/*
+DESCRIPTION: 
+Subscribes to the current and target locations of the orbot and sends the delta to the orbot.
+
+FUNCTIONS:
+void 	messageCallbackVicon(geometry_msgs::TransformStamped)
+	Subscribes to the orbot object on vicon at /vicon/orbot/orbot and assigns the pose to loc and att
+void	messageCallbackTarget(geometry_msgs::TransformStamped)
+	Subscribes to the target on /orbot_server/target and assigns the target pose to tarLoc and tarAtt
+int 	main(int, char**)
+	Starts the subscribers and publisher, publishes delta between target and current pose to the /orbot_server/orbot_delta
+
+NOTES:
+	Would like to move this functionality into the orbot client eventually, since the client should be able to subscribe
+	to both on its own. Only currently using this for debugging/separation of tasks
+
+Last edited by James Bell on 1/9/18
+*/
+
+#include <ros/ros.h>
+#include <geometry_msgs/Vector3.h>
+#include <geometry_msgs/TransformStamped.h>
+
+#include <iostream>
+#include <fstream>
+
+#include "quaternion.h"
+
+
+ros::Publisher pub;
+
+ros::Subscriber subCygnus, subCamera;
+ros::Time tCyg, tCam;
+Vec3 locCyg,locCam;
+Vec4 quatCyg,quatCam;
+void messageCallbackCygnus( geometry_msgs::TransformStamped t){
+  //const geometry_msgs::TransformStamped *t = &transform;
+  std::string a =  t.header.frame_id;//currently unused
+  tCyg = t.header.stamp;
+  //defined in quaternion.h
+
+  locCyg.v[0] = t.transform.translation.x;
+  locCyg.v[1] = t.transform.translation.y;
+  locCyg.v[2] = t.transform.translation.z;
+  quatCyg.v[0] = t.transform.rotation.w;
+  quatCyg.v[1] = t.transform.rotation.x;
+  quatCyg.v[2] = t.transform.rotation.y;
+  quatCyg.v[3] = t.transform.rotation.z;
+  // att = Quat2RPY(quat);
+
+
+}
+void messageCallbackCamera(geometry_msgs::TransformStamped t){
+  //const geometry_msgs::TransformStamped *t = &transform;
+  std::string a =  t.header.frame_id;//currently unused
+  tCam = t.header.stamp;
+  //defined in quaternion.h
+
+  locCam.v[0] = t.transform.translation.x;
+  locCam.v[1] = t.transform.translation.y;
+  locCam.v[2] = t.transform.translation.z;
+  quatCam.v[0] = t.transform.rotation.w;
+  quatCam.v[1] = t.transform.rotation.x;
+  quatCam.v[2] = t.transform.rotation.y;
+  quatCam.v[3] = t.transform.rotation.z;
+
+
+}
+
+int main(int argc, char** argv){
+	ros::init(argc,argv,"tsl_vicon");
+	ros::NodeHandle nh;
+  subCygnus=nh.subscribe("/vicon/cygnus_tsl/cygnus_tsl/",1000,messageCallbackCygnus);
+  subCamera=nh.subscribe("/vicon/camera_tsl/camera_tsl",1000,messageCallbackCamera);
+
+  std::ofstream out;
+  out.open("/home/James/vicon_out.txt",std::ofstream::out);
+  out <<"";
+  out.close();
+	
+	ros::Rate loop_rate(10);
+	while(ros::ok()){
+    if(tCyg.toNSec()==0)
+      continue;
+    out.open("/home/James/vicon_out.txt",std::ofstream::out | std::ofstream::app);
+    out << tCyg.toNSec()<<"\t"<<(double)(locCyg.v[0])<<"\t"<<(double)(locCyg.v[1])<<"\t"<<(double)(locCyg.v[2])<<"\t"<<(double)(quatCyg.v[0])<<"\t"<<(double)(quatCyg.v[1])<<"\t"<<(double)(quatCyg.v[2])<<"\t"<<(double)(quatCyg.v[3])<<"\n";
+    out.close();
+	  ros::spinOnce();
+	  loop_rate.sleep();
+	}
+	return 0;
+}
+
+
+
+
+
+
